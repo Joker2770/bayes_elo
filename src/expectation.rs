c1540634ea088d60c8f42b9014bd27872c640716
Joker2770 <1214220480@qq.com>
2024-07-19 19:00:24 +0800

    docs: add license header :bookmark:

--- src/expectation.rs
+++ src/expectation.rs
@@ -1,3 +1,21 @@
+/*
+A library for calculating Elo in balanced and not balanced competitions or games.
+Copyright (C) 2024  joker2770
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 pub mod elo_expectation {
     pub fn get_elo_expectation(delta: f64) -> f64 {
         1.0 / (1.0 + (10.0_f64.powf(delta / 400.0)))
